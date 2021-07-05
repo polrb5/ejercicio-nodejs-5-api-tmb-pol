84d8e2b924357437964704a160feec3aa47e5c75
Pol Reig <polreigbroto@gmail.com>
2021-07-05 17:16:18 +0200

    Servidor local levantado

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 require("dotenv").config();
+const debug = require("debug")("servidor:root");
 const fetch = require("node-fetch");
+const chalk = require("chalk");
 const express = require("express");
 const morgan = require("morgan");
+
+const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.json());
+
+const port = process.env.PORT;
+
+const server = app.listen(port, () => {
+  debug(`Servidor escuchando en el port ${chalk.yellow(port)}`);
+});
+
+server.on("error", (err) => {
+  debug(chalk.red("No se ha podido levantar el servidor"));
+  if (err.code === "EADDRINUSE") {
+    debug(chalk.red(`El port ${chalk.bold(port)} está ocupado`));
+  }
+});
